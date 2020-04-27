cd477703638243e13e9d4664b7a00a59ee684d1c
bakir1 <bakir_cutuk@hotmail.com>
2020-04-27 16:39:58 +0200

    test1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "./components/layouts/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
+//react router import
+import { Switch, Route } from "react-router-dom";
+//import pages
+import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/products" component={Products} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,101 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-warning">
+      <Link className="navbar-brand" href="#">
+        Online Bakery Shop
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item ">
+            <Link className="nav-link text-black text uppercase" to="/home">
+              Home&nbsp;<i class="fas fa-home"></i>{" "}
+              <span className="sr-only">(current)</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-black text uppercase" to="/products">
+              Products&nbsp;<i class="fas fa-bread-slice"></i>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-black text uppercase" to="/about">
+              About
+            </Link>
+          </li>
+
+          <li className="nav-item dropdown">
+            <Link
+              className="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Pages
+            </Link>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <Link className="dropdown-item" href="#">
+                Gallery
+              </Link>
+              <Link className="dropdown-item" href="#">
+                Blog
+              </Link>
+              <div className="dropdown-divider"></div>
+              <Link className="dropdown-item" href="#">
+                Tutorials
+              </Link>
+            </div>
+          </li>
+          <li className="nav-item">
+            <Link
+              className="nav-link disabled"
+              href="#"
+              tabindex="-1"
+              aria-disabled="true"
+            >
+              Receipts(Soon-Disabled)
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-black text uppercase" to="/contact">
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+        <form className="form-inline my-2 my-lg-0">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button
+            className="btn btn-outline-primary my-2 my-sm-0"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
